4b37654a0bba92c5c29a4a8cbea9844387453064
Jonny Moore <jonnymoore121212@gmail.com>
2016-07-20 14:27:53 +0100

    Ex9 Adds views dirctory and links html

--- app.rb
+++ app.rb
@@ -2,8 +2,10 @@ require 'sinatra'
 
 set :session_secret, 'super secret'
 
+#get '/secret' do
+#  "I won't tell anyone"
+#end
+
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-  <img src='http://bit.ly/1eze8aE'>
-  </div>"
+  erb(:index)
 end
